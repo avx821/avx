d5ddf7dfa3cfb54c140a343fe7361ef742ef3594
4thwest <mcole@mit.edu>
2015-04-16 10:29:37 -0400

    Adds updated navio data publishing with split imu and gps.

---
+++ src/imu_pub.cpp
@@ -0,0 +1,70 @@
+/*
+    This program is designed to collect data from the Navio
+    and pubish it via lcm to the channel imu. It uses the
+    navio_imu_t.lcm data type.
+*/
+
+//Inclusions for lcm
+#include "lcm/lcm-cpp.hpp"
+#include "avionics/navio_imu_t.hpp"
+
+//Inclusions for time
+#include <ctime>
+#include <sys/time.h>
+
+//Inclusions for IMU
+#include "Navio/MPU9250.h"
+
+unsigned long long get_timestamp(){
+	struct timeval tv;
+	gettimeofday(&tv,NULL);
+	unsigned long long millisecondsSinceEpoch=
+	(unsigned long long)(tv.tv_sec)*1000+
+	(unsigned long long)(tv.tv_usec)/1000;
+}
+
+using namespace std;
+
+int main(int argc, char ** argv)
+{
+    //initialize lcm
+    lcm::LCM lcm;
+    if(!lcm.good())
+        return 1;
+
+    //initialize IMU
+    MPU9250 imu;
+    imu.initialize();
+    float ax, ay, az, gx, gy, gz, mx, my, mz;
+
+    //Initialize data variable
+    avionics::navio_imu_t data;
+
+        while (true)
+        {
+		//Store time
+		unsigned long long timestamp;
+		timestamp = get_timestamp();
+		data.timestamp = timestamp;
+
+		//Store IMU data
+		imu.getMotion9(&ax, &ay, &az, &gx, &gy, &gz, &mx, &my, &mz);
+		data.imu_pos[0] = mx;
+		data.imu_pos[1] = my;
+		data.imu_pos[2] = mz;
+		data.imu_vel[0] = gx;
+		data.imu_vel[1] = gy;
+		data.imu_vel[2] = gz;
+		data.imu_acc[0] = ax;
+		data.imu_acc[1] = ay;
+		data.imu_acc[2] = az;
+
+	    //Publish it!
+	    lcm.publish("imu", &data);
+
+	    //Default IMU sleep duration
+	    sleep(0.05);
+	}
+
+    return 0;
+}
