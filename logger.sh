648ebacfabb77387d62522baaa7d15e3bcebe461
shang418 <shang418@mit.edu>
2015-04-16 19:31:00 +0000

    Updated logger.sh

--- logger.sh
+++ logger.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 sudo modprobe bcm2835-v4l2
-../Downloads/lcm/lcm-logger/lcm-logger& 
-ant
+./../../Downloads/lcm/lcm-logger/lcm-logger& 
+ant&
+./~/avx/src/gps_pub&
+./~/avx/src/imu_pub
