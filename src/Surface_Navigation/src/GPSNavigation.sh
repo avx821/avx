7ce533c926f28f4ad60477fac4f57ae127673148
AdmiralLima <nicklima@mit.edu>
2015-04-30 05:55:40 +0000

    changes to surface nav

--- src/Surface_Navigation/src/GPSNavigation.sh
+++ src/Surface_Navigation/src/GPSNavigation.sh
@@ -4,6 +4,9 @@ javac -cp lcm.jar:. avionics/*.java
 jar cvf avionics.jar avionics/*.class
 javac -cp lcm.jar:avionics.jar:. *.java
 echo "starting imu publishing"
-./../../../imu_pub &
+./../../imu_pub &
 echo "running GPS Nav code"
+./../../gps_pub &
+echo "running motor listener"
+sudo ./../motor/motor_controler & 
 java -cp lcm.jar:avionics.jar:. GPS_Navigation 60.000 12.0001 20.0
