0e0e2aa10aef1fc216a63021ad0bd149d34e55c1
4thwest <mcole@mit.edu>
2015-05-01 11:36:41 -0400

    AC changes to GS2P

--- src/comms/GS2Plane.py
+++ src/comms/GS2Plane.py
@@ -4,8 +4,8 @@ from serial import Serial
 from xbee import ZigBee
 
 #Serial to LCM Mapping
-TxLCMMap = ["05EBOLA","imu","gps"]  #LCM Messages to receive
-RxLCMMap = ["05EBOLA","OVERRIDE","pod_data"] 	#LCM Messages to Transmit
+TxLCMMap = ["05EBOLA","imu","gps"]  #LCM Messages to receive from plane
+RxLCMMap = ["05EBOLA","OVERRIDE","pod_data"] 	#LCM Messages to Transmit to plane
 #Setup Handler for LCM
 lc = lcm.LCM()
 channelDB = dict()
@@ -14,29 +14,34 @@ channelDB = dict()
 #On Windows, uncomment
 #Port = 'COM3'
 #On Unix/Linux, uncomment
-PORT = '/dev/ttyUSB0'
+PORT = '/dev/ttyUSB1'
 BAUD = 9600
 ser = Serial(PORT, BAUD)
 
 def serialHandler(serialData):
+    print serialData
     if 'rf_data' in serialData:
+        print serialData['rf_data']
         serialData = str(serialData['rf_data'])
         #Handler converting Serial to LCM
-        (channelNum, data) = serialData.split('.')
+        print serialData
+        #(channelNum, data) = serialData.split('.')
+        channelNum = serialData[0]
+        data = serialData[2:]
         channelNum = int(channelNum)
-        channelNameLCM = PREFIX+SLCMMap[channelNum]
+        channelNameLCM = TxLCMMap[channelNum]
         lc.publish(channelNameLCM,data)
 
 xbee = ZigBee(ser, callback=serialHandler)
 
 def lcmHandler(channelNameLCM, data):
     #Handler converting LCM to Serial
-    channelNum = TxLCMMap.index(channelNameLCM)
+    channelNum = RxLCMMap.index(channelNameLCM)
     #xbee.tx(dest_addr='\x00\x01', data='%s.%s' % (str(channelNum), data))
     xbee.tx(dest_addr_long='\x00\x13\xA2\x00\x40\xD6\x63\xBB',dest_addr='\xFF\xFE',data='%s.%s' % (str(channelNum), data) )
     print "LCM!"
 
-subscriptions = [lc.subscribe(name,lcmHandler) for name in TxLCMMap]
+subscriptions = [lc.subscribe(name,lcmHandler) for name in RxLCMMap]
 
 print 'setup complete'
 
