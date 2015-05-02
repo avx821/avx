d9ee203362258804b783f4d0824f49faaad889cd
AdmiralLima <nicklima@mit.edu>
2015-05-02 07:26:28 +0000

    resolved local repo conflicts

--- src/Surface_Navigation/src/GPSNavigation.sh
+++ src/Surface_Navigation/src/GPSNavigation.sh
@@ -10,4 +10,5 @@ echo "starting imu publishing"
 ./../../gps_pub &
 #echo "running motor listener"
 sudo ./../../motor/motor_controler & 
-java -cp lcm.jar:avionics.jar:. GPS_Navigation 42.359 -72.09 60.0 42.359 -71.5000
+#./../../../../Downloads/lcm/lcm-logger
+java -cp lcm.jar:avionics.jar:. GPS_Navigation 42.359 -72.09 60.0
