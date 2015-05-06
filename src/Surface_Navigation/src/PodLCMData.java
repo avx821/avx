8c4827ba2ba1d9163b7840768d44df62c3fa9428
shang418 <shang418@mit.edu>
2015-05-06 09:26:09 -0400

    added pod comms capability

--- src/Surface_Navigation/src/PodLCMData.java
+++ src/Surface_Navigation/src/PodLCMData.java
@@ -1,34 +1,31 @@
 
-import java.io.IOException; import avionics.*; import lcm.lcm.*;
+import java.io.IOException; 
+import avionics.*; 
+import lcm.lcm.*;
 
-public class LCMData implements LCMSubscriber,Runnable {
+public class PodLCMData implements LCMSubscriber,Runnable {
 		 LCM GPSdata_LCM,Heading_LCM;  
+		 //TODO change message types
 		 navio_gps_t GPS_msg;
 		 navio_imu_t Heading_msg;
-		 long latitude; 
-		 long longitude; 
-		 long altitude;
+		 double latitude; 
+		 double longitude; 
+		 double altitude;
 		 double heading=Double.NaN;
 		 boolean connected=false;
-		 MovingFilter filterx,filtery;
 		 GPS_Navigation gpsnav;
 		final double _tolerance=1E-3;
-		public LCMData() throws IOException {
+
+		public PodLCMData() throws IOException {
 			this.GPSdata_LCM = new LCM();
 			this.Heading_LCM = new LCM();
 	        this.GPSdata_LCM.subscribe("gps", this);
 	        this.Heading_LCM.subscribe("imu", this);
-	        this.filterx=new MovingFilter();
-		this.filtery=new MovingFilter();
 		this.connected=false;
 	      /*
 	       * Do here-write more code
 	       *  // initialize heading and GPS message from last mode
 	       */
-		}
-		--> make gps files
-		public void setNav(GPS_Navigation nav){
-			this.gpsnav=nav;
 		}
 		public double getLongitude() {
 			return longitude;
@@ -36,12 +33,10 @@ public class LCMData implements LCMSubscriber,Runnable {
 		public double getLatitude() {
 			return latitude;
 		}
-		public double getAltitude() {
-			return altitude;
-		}
 		public double getHeading() {
 			return heading;
 		}
+
 		public double[] getIMU_pos(){
 		double x=(double) (Heading_msg.imu_pos[0]/1.0);
 		double y=(double) (Heading_msg.imu_pos[1]/1.0);
@@ -51,13 +46,13 @@ public class LCMData implements LCMSubscriber,Runnable {
 
 		public synchronized void messageReceived(LCM lcm, String channel, LCMDataInputStream ins){
 		    //  System.out.println("Received message on channel:  " + channel); 
+		    //TODO: changegd message channel names
 		        try {
 		            if (channel.contains("gps")) {
 		            	 GPS_msg=new navio_gps_t(ins);
 		        //    	 System.out.println("  timestamp    = " + GPS_msg.timestamp+ "Got GPS data");
 			             this.latitude=GPS_msg.lat; 
 			             this.longitude=GPS_msg.lon; 
-			             this.altitude=GPS_msg.alt;
 		            	}
 		            if (channel.contains("imu")) {
 		            	Heading_msg=new navio_imu_t(ins);
@@ -79,27 +74,19 @@ public class LCMData implements LCMSubscriber,Runnable {
 			return connected;
 		}
 		public double calculateHeading() {
-			long[] imu_pos=Heading_msg.imu_pos; 
-			
-			double magx=(double) (-0.04691*imu_pos[0])-0.045167;
-			double magy=(double) (-0.04194*imu_pos[1])+1.2;
-			magx=this.filterx.getAverage(magx);
-			magy=this.filtery.getAverage(magy);
-			
-			double magheading= -Math.atan2(magy,magx)-(Math.PI/2.0) ;
-			System.out.println("[magx,magy]:["+magx+", "+magy+"]");
+			double magheading= Heading_msg.heading-Math.PI();
 			return magheading;
 		}
 		@Override
 	public void run() {
-	//public static void main(String[] args){
-	System.out.println("waiting for comm. startup");
+	
+	System.out.println("waiting for pod comm. startup");
 	synchronized(gpsnav){
 		while(!connected){
 			try{
 			gpsnav.wait();
 			}catch(InterruptedException ex){
-			System.out.println("Monitor exxception");
+			System.out.println("Monitor exception");
 			}
 		}
 		System.out.println("Stop waiting");
